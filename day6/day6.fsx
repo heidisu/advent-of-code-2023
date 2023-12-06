0ce3db5b544716640b3387a81c14995cd1e11e21
Heidi Mork <heidi.mork@nrk.no>
2023-12-06 08:32:00 +0100

    tidy up

--- day6/day6.fsx
+++ day6/day6.fsx
@@ -5,7 +5,7 @@ let readFile () =
     File.ReadLines "input.txt"
     |> Seq.toList
 
-let parsemultiNumberLine (l: string) = 
+let parseMultiNumberLine (l: string) = 
     (l.Split(":")[1]).Trim().Split(" ")
         |> Array.filter (fun s -> s <>"")
         |> Array.map int64
@@ -20,8 +20,8 @@ let parseSingleNumberLine (l: string) =
     |> int64
 
 let parse (l1: string) (l2: string) = 
-    let times = parsemultiNumberLine l1
-    let records = parsemultiNumberLine l2
+    let times = parseMultiNumberLine l1
+    let records = parseMultiNumberLine l2
     List.zip times records
 
 let parse2 (l1: string) (l2: string): int64 * int64 = 
@@ -29,24 +29,24 @@ let parse2 (l1: string) (l2: string): int64 * int64 =
     let records = parseSingleNumberLine l2
     (times, records)
 
-let calculcateRecord (time: int64) (record: int64) = 
-    [1 .. (int time)] 
+let calculateRecords (time: int64) (record: int64) = 
+    [1 .. (int time)]
+    |> List.map int64
     |> List.map (fun charge ->
-        let c = int64 charge
-        let remaining = time - c
-        remaining * c)
+        let remaining = time - charge
+        remaining * charge)
     |> List.filter (fun i -> i > record)
     |> List.length
 
 let task1 = 
     let lines = readFile ()
     parse lines[0] lines[1]
-    |> List.map (fun (t, r) -> calculcateRecord t r)
+    |> List.map (fun (t, r) -> calculateRecords t r)
     |> List.fold ( * ) 1
 let task2 = 
     let lines = readFile ()
     let (time, record) = parse2 lines[0] lines[1]
-    calculcateRecord time record
+    calculateRecords time record
 
 printfn $"Task 1: {task1}" //449550
 printfn $"Task 2: {task2}" // 28360140
