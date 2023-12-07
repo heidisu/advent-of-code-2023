9d45b8b3b80b25d61cf8183b730d3275196b462a
Heidi Mork <heidi.mork@nrk.no>
2023-12-07 10:23:36 +0100

    day 7 part 2

--- day7/day7.fsx
+++ day7/day7.fsx
@@ -90,11 +90,49 @@ let getType (cards: Card list) =
     else if numGroups = 4 then OnePair
     else failwith "not valid"
 
+// 5J, 4 J -> FiveOfAKind
+// 3J -> Four
+// 2J -> 2J + A B C = Tree
+// 2J  + 2A + B
+// 2J 3B -> Five
+let getType2 (cards: Card list) =
+    let groups = cards |> List.groupBy id
+    match List.tryFind (fun (c, l) -> c = J) groups with
+    | Some (c, l) ->
+        let size = List.length l
+        if size = 4 || size = 5 then FiveOfAKind
+        else if size = 3 then
+            if List.length cards = 2 then FiveOfAKind
+            else FourOfAKind
+        else if size = 2 then
+            let groupSize = List.length groups
+            printfn "groupsize %A" groupSize
+            if groupSize = 4 then ThreeOfAKind
+            else if groupSize = 3 then FourOfAKind
+            else if groupSize = 2 then FiveOfAKind
+            else FiveOfAKind
+        else if size = 1 then
+            let indexJ = List.findIndex (fun c -> c = J) cards
+            [A; K; Q; T; Nine; Eight; Seven; Six; Five; Four; Three; Two]
+            |> List.map (fun c ->
+                    cards
+                    |> List.insertAt indexJ c
+                    |> List.removeAt (indexJ + 1))
+            |> List.map (fun cards ->
+                    let cardType = getType cards
+                    (cardType, typeToValue cardType))
+            |> List.sortBy snd
+            |> List.rev
+            |> List.head
+            |> fst
+        else failwith "error"
+    | None -> getType cards
+    
 let readFile () = 
     File.ReadLines "input.txt"
     |> Seq.toList
 
-let parse (l: string) =
+let parse (getType: Card list -> CardType) (l: string) =
     let parts = l.Split(" ")
     let bid = parts[1] |> int64
     let cards = parts[0] |> Seq.map charToCard |> Seq.toList
@@ -114,13 +152,20 @@ let comparer (first: Hand) (second: Hand) =
         match List.tryFind (fun (f, s) -> f <> s) zipped with
         | Some (c1, c2) -> if cardToValue c1 > cardToValue c2 then 1 else -1
         | None -> 0
+
 let task1 =
     readFile ()
-    |> List.map parse
+    |> List.map (parse getType)
     |> List.sortWith comparer
     |> List.mapi (fun i h -> (int64 (i + 1)) * h.Bid)
     |> List.sum
-let task2 = "task 2"
+let task2 =
+    readFile ()
+    |> List.map (parse getType2)
+    |> List.sortWith comparer
+    |> List.mapi (fun i h -> (int64 (i + 1)) * h.Bid)
+    |> List.sum
+   //|> List.iter (printfn "%A")
 
 printfn $"Task 1: {task1}" // 253954294
-printfn $"Task 2: {task2}"
+printfn $"Task 2: {task2}" // 253647164 too low // 254917592 too high
