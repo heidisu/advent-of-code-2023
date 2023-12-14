3fb6e87f000f97bb81ea01bf31d0508783477ea8
Heidi Mork <heidi.mork@nrk.no>
2023-12-14 16:54:59 +0100

    day 14

---
+++ day14/day14.fsx
@@ -0,0 +1,123 @@
+﻿open System.Data
+open System.IO
+
+type Direction = | North | West | South | East
+
+let readFile () = 
+    File.ReadLines "input.txt"
+    |> Seq.toArray
+    |> Array.map (Seq.toArray)
+
+let print grid = 
+    grid
+    |> Array.iter(fun row ->
+            row |> Array.iter(fun c -> printf "%c" c)
+            printfn ""
+        )
+
+let rec moveRocks (prev: char array) (row: char array) = 
+    if prev = row then row
+    else
+        let stoneIdxs = 
+            row
+            |> Array.mapi (fun i c -> (i, c))
+            |> Array.filter (fun (_, c) -> c ='O')
+            |> Array.map fst
+        let newRow = 
+            stoneIdxs
+            |> Array.fold (fun (arr: char array) i -> 
+                if i - 1 >= 0 && arr[i - 1] = '.' then
+                    arr
+                    |> Array.updateAt (i - 1) 'O'
+                    |> Array.updateAt i '.'
+                else arr) row
+        moveRocks row newRow
+
+let rotate (direction: Direction) width height (grid: char array array) =
+    match direction with
+    | North ->
+        [|0 .. width - 1|]
+        |> Array.map (fun idx -> 
+            [|0 .. height - 1|]
+            |> Array.map (fun i -> grid[i][idx]))
+    | West -> grid
+    | South ->
+        [|0 .. width - 1|]
+        |> Array.map (fun idx -> 
+            [|0 .. height - 1|]
+            |> Array.map (fun i -> grid[i][idx]) |> Array.rev) |> Array.rev
+    | East ->
+        grid
+        |> Array.map Array.rev    
+
+let rotateBack (direction: Direction) width height (grid: char array array) =
+    match direction with
+    | North -> rotate North width height grid
+    | West -> grid
+    | South -> rotate South width height grid
+    | East -> rotate East width height grid
+    
+
+let northSupportBeams width height (grid: char array array) =
+    let size = Array.length grid[0]
+    grid
+    |> rotate North width height
+    |> Array.map (
+        fun row -> 
+            row 
+            |> Array.mapi (fun i c -> if c = 'O' then int64 (size - i) else 0L)
+            |> Array.sum
+            )
+    |> Array.sum
+
+let move (direction: Direction) (grid: char array array) = 
+    let width = Array.length grid[0]
+    let height = Array.length grid 
+    let moved = 
+        rotate direction width height grid
+        |> Array.map (fun row -> moveRocks [||] row)
+    
+    let back = rotateBack direction width height moved
+    (northSupportBeams width height back, back)
+
+let grid = readFile ()
+
+let cycle = [North; West; South; East]
+
+let moveCycle (grid: char array array) =
+    cycle
+    |> List.fold (fun (_, snd) d ->
+        move d snd
+        ) (0L, grid)
+
+let findCycle (grid: char array array) =
+    let mutable found  = false
+    let mutable idx = 1
+    let mutable prev = grid
+    let mutable beams = []
+    let mutable grids = [ (1, grid) ]
+    let mutable result = 0L
+    while not found do
+        let (beam, gr) = moveCycle prev
+        let find = List.tryFind (fun (i, g) -> g = gr) grids
+        match find with
+        | Some (i, g) -> 
+            found  <- true
+            prev <- gr
+            let correct = (1000000000 - i) % (idx - i) + i
+            result <-
+                beams
+                |> List.find (fun (i, g) -> i = correct)
+                |> snd
+        | None ->
+            prev <- gr
+            beams <- (idx, beam) :: beams
+            grids <- (idx, gr) :: grids
+            idx <- idx + 1
+    result
+    
+let task1 =  move North grid |> fst
+let task2 = findCycle grid
+
+printfn $"Task 1: {task1}" // 110677
+printfn $"Task 2: {task2}" // 90551
